944394445ddc5255f733233790cea27f26292722
JFelixZuniga <johnfzuniga@gmail.com>
2021-02-16 22:03:09 -0300

    2do commit, leer moneda y criptomoneda, además de validar

---
+++ js/app.js
@@ -0,0 +1,83 @@
+const criptomonedasSelect = document.querySelector("#criptomonedas");
+const monedaSelect = document.querySelector("#moneda");
+const formulario = document.querySelector("#formulario");
+
+const objBusqueda = {
+  moneda: "",
+  criptomoneda: "",
+};
+
+// Crear un Promise
+const obtenerCriptomonedas = (criptomonedas) =>
+  new Promise((resolve) => {
+    resolve(criptomonedas);
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  consultarCritomonedas();
+
+  formulario.addEventListener("submit", submitFormulario);
+
+  criptomonedasSelect.addEventListener("change", leerValor);
+  monedaSelect.addEventListener("change", leerValor);
+});
+
+function consultarCritomonedas() {
+  const url =
+    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((resultado) => obtenerCriptomonedas(resultado.Data))
+    .then((criptomonedas) => selectCriptomonedas(criptomonedas));
+}
+
+function selectCriptomonedas(criptomonedas) {
+  criptomonedas.forEach((cripto) => {
+    const { FullName, Name } = cripto.CoinInfo;
+
+    const option = document.createElement("OPTION");
+    option.value = Name;
+    option.textContent = FullName;
+
+    criptomonedasSelect.appendChild(option);
+  });
+}
+
+function leerValor(e) {
+  objBusqueda[e.target.name] = e.target.value;
+  console.log(objBusqueda);
+}
+
+function submitFormulario(e) {
+  e.preventDefault();
+
+  // Validar
+  const { moneda, criptomoneda } = objBusqueda;
+
+  if (moneda === "" || criptomoneda === "") {
+    mostrarAlerta("Ambos campos son obligatorios");
+    return;
+  }
+
+  // Consultar la API con los resultados
+}
+
+function mostrarAlerta(msg) {
+  const existeError = document.querySelector(".error");
+
+  // Si no existe el error
+  if (!existeError) {
+    const divMensaje = document.createElement("DIV");
+    divMensaje.classList.add("error");
+
+    // Mensaje de error
+    divMensaje.textContent = msg;
+
+    formulario.appendChild(divMensaje);
+
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
